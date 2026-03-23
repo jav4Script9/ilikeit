d564245e378c3dcf591e5a744460849987e2d3cd
Lionez <jav4script9@users.noreply.github.com>
2026-03-23 15:30:14 +0100

    fix supabase config

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Замени на свои данные из Supabase Dashboard → Settings → API
-const SUPABASE_URL = 'https://ТВОЙ_ПРОЕКТ.supabase.co'
-const SUPABASE_ANON_KEY = 'ТВОЙ_ANON_KEY'
+const SUPABASE_URL = 'https://oeuskmkmbrdqodreypuk.supabase.co'
+const SUPABASE_ANON_KEY = 'sb_publishable__KXR9etMdRLn8wplffIHQw_YlgbbnPY'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
